5f37ec3662f017a98338b63bbec197bfe19bc6c1
David <184929742+davidsangouard@users.noreply.github.com>
2025-12-12 10:59:57 +0100

    Add files via upload

---
+++ save.bat
@@ -0,0 +1,6 @@
+C:\xampp\mysql\bin\mysqldump -uroot techsolutions>C:\xampp\htdocs\techsolutions\BDD\techsolutions.sql
+cd C:\xampp\htdocs\techsolutions
+git add .
+git commit -m "save"
+git push origin main
+pause
